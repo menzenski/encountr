d01da05f214343f05b1279d78d27c097c8731336
Matt Menzenski <matt@mobilgov.com>
2018-09-30 14:25:05 -0500

    pull out CLI into its own namespace

--- src/encountr/core.clj
+++ src/encountr/core.clj
@@ -1,24 +1,10 @@
 (ns encountr.core
-  (:require [clojure.tools.cli :as cli]))
-
-(def cli-options
-  [["-k" "--kind KIND" "Kind"]
-   ["-n" "--name NAME" "Name"]
-   ["-c" "--count COUNT" "Count"
-    :parse-fn #(Integer/valueOf %)]
-   ["-r" "--[no-]roll" "Roll rather than use default values"]
-   ["-h" "--help" "Show help"]])
+  (:require [encountr.cli.core :as cli]))
 
 (defn- main
   "Generate a combat encounter from the provided options.
 
   `lein run -- monster -k Bugbear -n Steve -c 2 monster -k Owlbear -r`"
   [& args]
-  (let [{:keys [options arguments errors summary] :as parsed-options} (cli/parse-opts args cli-options :in-order true)]
-    (println (with-out-str (clojure.pprint/pprint options)))
-    (cond
-      (:help options) (println summary)
-      :else (condp = (some-> arguments first keyword)
-              :monster (apply main (rest arguments))
-              (println summary)))))
+  (apply cli/main args))
 
