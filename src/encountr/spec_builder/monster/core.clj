39c025f39c690dc128ec1dc2f5e6a3bf7bceacee
Matt Menzenski <matt@mobilgov.com>
2018-10-06 22:37:13 -0500

    rationalize challenge ratiing to handle 0.5 etc

--- src/encountr/spec_builder/monster/core.clj
+++ src/encountr/spec_builder/monster/core.clj
@@ -3,7 +3,7 @@
 
 (defn monster*
   [{:keys [_id index name size alignment armor_class hit_points hit_dice url challenge_rating initiative] :as all}]
-  (spec/conform all))
+  (spec/conform (assoc all :challenge_rating (rationalize challenge_rating))))
 
 (defn monster
   [& {:keys [_id index name size alignment armor_class hit_points hit_dice url challenge_rating initiative] :as all}]
